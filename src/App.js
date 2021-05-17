0ea3bef2f7e4a0a947e134ffec927a998092b1f7
ryanparker196 <ryanparker196@gmail.com>
2021-05-17 18:13:16 -0400

    fixed iframe

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h2>Community Detection Between Soundcloud Artists</h2>
         <p>Nodes are SoundCloud User IDs, edges are combined interaction (likes, follows, reposts, comments). Colors correspond to communities assigned by the Louvain method, node labels are scaled by in-degree.</p>
-        <iframe allowfullscreen="true" src="https://www.easyzoom.com/embed/94341e98e8d94eb4923b5f21657fb768" width="90%" height="750"></iframe>
+        <iframe title="Data-Visualization" allowfullscreen="true" src="https://www.easyzoom.com/embed/94341e98e8d94eb4923b5f21657fb768" width="90%" height="750"></iframe>
       </header>
     </div>
   );
